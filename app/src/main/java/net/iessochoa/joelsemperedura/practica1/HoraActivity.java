e7e1b5deae2166dece4c0758e3043294f8c4c288
JoelS <joelsempere96@gmail.com>
2021-09-23 19:35:33 +0200

    Programa funcionando a falta de añadir comentarios

--- app/src/main/java/net/iessochoa/joelsemperedura/practica1/HoraActivity.java
+++ app/src/main/java/net/iessochoa/joelsemperedura/practica1/HoraActivity.java
@@ -2,13 +2,32 @@ package net.iessochoa.joelsemperedura.practica1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Date;
 
 public class HoraActivity extends AppCompatActivity {
+    public static final String EXTRA_HORA="HoraActivity.HORA";
+
+    TextView mensaje;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hora);
+
+        String hora = getIntent().getStringExtra(EXTRA_HORA);
+        String nombre = getString(R.string.stMiNombre);
+        mensaje=findViewById(R.id.tvHoraSaludo);
+
+        String recurso = getResources().getString(R.string.string_formateada);
+        String stringFormateada = String.format(recurso,hora,nombre);
+
+        mensaje.setText(stringFormateada);
+
+
+
     }
 }
--- app/src/main/java/net/iessochoa/joelsemperedura/practica1/MainActivity.java
+++ app/src/main/java/net/iessochoa/joelsemperedura/practica1/MainActivity.java
@@ -1,18 +1,25 @@
 package net.iessochoa.joelsemperedura.practica1;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.icu.util.LocaleData;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class MainActivity extends AppCompatActivity {
     EditText etNombre;
     Button btnOk;
     Button btnHora;
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +35,11 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
-        btnHora.setOnClickListener(v -> enviaHora());
+        btnHora.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, HoraActivity.class);
+            intent.putExtra(HoraActivity.EXTRA_HORA,enviaHora());
+            startActivity(intent);
+        });
 
     }
 
@@ -38,8 +49,11 @@ public class MainActivity extends AppCompatActivity {
         btnHora=findViewById(R.id.btnHora);
     }
 
-    private void enviaHora() {
-        //RELLENAR
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private String enviaHora() {
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("HH:mm:ss");
+        String hora=df.format(LocalDateTime.now());
+        return hora;
     }
 
 }
